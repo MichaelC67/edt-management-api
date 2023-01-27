1a433dfe85f0ff6eed517dad84bae52f6b001c81
Michael Canales <michael.canales@insee.fr>
2023-01-27 15:11:29 +0100

    Add cors

---
+++ src/main/java/fr/insee/edtmanagement/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package fr.insee.edtmanagement.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration(proxyBeanMethods = false)
+public class CorsConfig {
+
+	@Value("${fr.insee.edtmanagement.corsAllowedOrigins}")
+	private String corsAllowedOrigins;
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry. addMapping("/**")
+						.allowCredentials(true)
+				        .allowedOrigins(corsAllowedOrigins)
+				        .allowedHeaders("*")
+				        .allowedMethods("*")
+				        .maxAge(3600L);
+			}
+
+		};
+	}
+
+}
